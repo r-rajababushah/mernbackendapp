f941d54a6b00bf74be8e21d3243907f9c76aea63
Rajababu Shah <rajababushah.in@gmail.com>
2023-07-29 14:05:47 +0530

    m

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 6010;
+const router = require("./Routes/router");
+require("./db/conn.js");
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+app.use("/uploads", express.static("./uploads"));
+app.use("/files", express.static("./public/files"));
+
+app.listen(PORT, () => {
+    console.log(`Server start at port no ${PORT}`);
+})
